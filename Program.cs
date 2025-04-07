5bcf36f6221ef40f73c7339da35278f7f9ad6d6b
M-fathirizky <fathirrizky636@gmail.com>
2025-04-08 01:18:01 +0800

    Menambahkan class KuliahMahasiswa103022300009

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        DataMahasiswa103022300009 data = new DataMahasiswa103022300009();
+
+        data.ReadJSON();
+
+        Console.WriteLine("");
+
+        DataMahasiswa103022300009 dataKuliah = new DataMahasiswa103022300009();
+
+        dataKuliah.ReadJSON();
+    }
+}
