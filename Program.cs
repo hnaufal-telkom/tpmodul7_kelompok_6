9fcde8183f82dc6ed44936772f6c2d7c963f4e79
hnaufal-telkom <hnaufal@student.telkomuniversity.ac.id>
2025-04-08 10:50:55 +0700

    Revert "merge branch naufal"

--- Program.cs
+++ Program.cs
@@ -1,15 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-
-class Program {
-  public static void Main(string[] args) {
-    DataMahasiswa103022300163 data = new DataMahasiswa103022300163();
-
-    data.ReadJSON();
-    
-    Console.WriteLine("");
-
-    KuliahMahasiswa103022300163 dataKuliah = new KuliahMahasiswa103022300163();
-
-    dataKuliah.ReadJSON();
-  }
-}
+Console.WriteLine("Hello, World!");
